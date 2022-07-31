28befd068301d5b7b3919dae27ab94a7f2f7901a
RinKit6024 <butasan6024@gmail.com>
2022-07-31 15:54:51 +0900

    First

---
+++ src/App.java
@@ -0,0 +1,9 @@
+import Class.FastScanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        FastScanner fsc = new FastScanner();
+        int N = fsc.nextInt();
+
+    }
+}
