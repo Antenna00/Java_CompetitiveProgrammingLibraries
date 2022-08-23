438dce427603ca94e50c34ee26cc1e97a396d419
RinKit6024 <butasan6024@gmail.com>
2022-08-23 22:10:26 +0900

    aa

--- src/Class/Note.java
+++ src/Class/Note.java
@@ -1,5 +1,27 @@
 package Class;
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
 public class Note {
 
+    public static void test() throws IOException {
+        NumberFormat nf = NumberFormat.getCurrencyInstance();
+
+        String t = Double.toString(12324.134);
+
+        String result = nf.format(t);
+
+        System.out.println(result);
+    }
 }
